80e61de5187357e646ee02851159f7b235d0a8f7
tentative13 <alex.regn@gmail.com>
2015-02-10 22:05:55 +0500

    Add migration Part of validation

---
+++ TwoFactorAuth/Models/Domain/BusinessCard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class BusinessCard
+    {
+        [ScaffoldColumn(false)]
+        public int ID { get; set; }
+        [ScaffoldColumn(false)]
+        public DateTime Created { get; set; }
+        [ScaffoldColumn(false)]
+        public DateTime ChangedDate { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Position { get; set; }
+        public byte[] CompanyLogo { get; set; }
+        public string[] KeyWords { get; set; }
+        public string WebSite { get; set; }
+        public byte[] BusinessDemo { get; set; }
+
+
+        public virtual ICollection<Address> Address { get; set; }
+        public virtual Persona Persona { get; set; }
+    }
+}
+
